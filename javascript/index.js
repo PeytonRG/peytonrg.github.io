1a1b16cb6f1bdd31acd3ddf6a9cf5b04ccf92e04
Peyton Gasink <peytongasink@att.net>
2020-05-31 17:07:04 -0500

    began working on glide.js carousel for project cards
    now we're cooking with gas

--- javascript/index.js
+++ javascript/index.js
@@ -31,4 +31,43 @@ $(document).ready(function () {
         await delay(250);
         AOS.refresh();
     }
+
+    var glide = new Glide('.glide', {
+        bound: true,
+        gap: 10,
+        peek: { before: 30, after: 30 },
+        perView: 1,
+        rewind: false,
+        startAt: 0,
+        swipeThreshold: 50,
+        type: 'slider',
+        // breakpoints: {
+        //     1199: {
+        //         perView: Math.min(navBarLinksCount, 6)
+        //     },
+        //     991: {
+        //         perView: Math.min(navBarLinksCount, 5)
+        //     },
+        //     768: {
+        //         gap: 10,
+        //         peek: { before: 75, after: 75 },
+        //         perView: Math.min(navBarLinksCount, 3),
+        //         type: 'carousel'
+        //     },
+        //     700: {
+        //         gap: 10,
+        //         peek: { before: 100, after: 100 },
+        //         perView: Math.min(navBarLinksCount, 2),
+        //         type: 'carousel'
+        //     },
+        //     550: {
+        //         gap: 10,
+        //         peek: { before: 90, after: 90 },
+        //         perView: 1,
+        //         type: 'carousel'
+        //     }
+        // }
+    });
+
+    glide.mount();
 });
